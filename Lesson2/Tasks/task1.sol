ad4b0603a5a7952cb54ce02a75104430f2d25212
thiagorochatr <thiagord2016@gmail.com>
2023-02-14 00:29:35 -0300

    improve task1 lesson2

--- Lesson2/Tasks/task1.sol
+++ Lesson2/Tasks/task1.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.8.0;
 
 contract DeleteFromDynamicArray {
-    uint[] public array;
+    uint8[] public array;
 
-    function createArray(uint[] memory itemsArray) public {
+    function createArray(uint8[] memory itemsArray) public {
         for (uint i = 0; i < itemsArray.length; i++) {
             array.push(itemsArray[i]);
         }
     }
 
-    function getArray() view public returns (uint[] memory) {
+    function getArray() external view returns (uint8[] memory) {
         return array;
     }
 
@@ -17,23 +17,24 @@ contract DeleteFromDynamicArray {
         delete array;
     }
 
-    function r1deleteItem(uint index) public {
-        uint len = array.length;
-        require(index < len, "Index out of bounds.");
+    // Keeping the array sorted, with array.pop()
+    function f1deleteItem(uint8 _index) external {
+        require(_index < array.length, "Index out of bounds.");
         
         // Move all items to the right of the deleted item one position to the left.
-        for (uint i = index; i < len - 1; i++) {
-            array[i] = array[i + 1];
+        for (; _index < array.length - 1; ++_index) {
+            array[_index] = array[_index + 1];
         }
 
         // Resize the array.
         array.pop();
     }
 
-    function r2deleteItem(uint256 index) external {
+    // Keeping the array sorted, with assembly
+    function f2deleteItem(uint8 _index) external {
         unchecked {
-            for (; index < array.length - 1; ++index){
-                array[index] = array[index + 1];
+            for (; _index < array.length - 1; ++_index){
+                array[_index] = array[_index + 1];
             }
             assembly {
                 sstore(array.slot, sub(sload(array.slot), 1))
@@ -41,16 +42,13 @@ contract DeleteFromDynamicArray {
         }
     }
 
-    // function r3deleteItem(uint256 index) external {
-    //     unchecked {
-    //         for (; index < array.length - 1; ++index){
-    //             assembly {
-    //                 let nextIndex := add(index, 1)
-    //                 let nextValue := sload(add(array.slot, nextIndex))
-    //                 sstore(add(array.slot, index), nextValue)
-    //             }
-    //         }
-    //         assembly { sstore(array.slot, sub(sload(array.slot), 1)) }
-    //     }
-    // }
+    // Not keeping the array sorted
+    function f3deleteItem(uint8 _index) external {
+        //write function in unchecked solidity
+        unchecked {
+            array[_index] = array[array.length - 1];
+            array.pop();
+        }
+    }
+
 }
